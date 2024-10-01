e980de44b2dfdb81c9c66c7a3eccd81954b1ecd0
MatheusGmota <livematgreg@gmail.com>
2024-10-01 20:26:40 -0300

    Criação do cabeçalho

---
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,9 @@
+import Menu from "../Menu/Menu";
+
+export default function Cabecalho() {
+  return (
+    <header className="fixed flex justify-center items-center w-full h-20 p-3 bg-slate-800">
+        <Menu/>
+    </header>
+  )
+}
