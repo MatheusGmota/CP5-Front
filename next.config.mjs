d092f18269bbfb11e2812216cdf0c4f71a03deff
MatheusGmota <livematgreg@gmail.com>
2024-10-01 20:38:05 -0300

    Configurando api da nasa images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns: [
+            {protocol: 'https', hostname: 'http.cat', port: '', pathname: '/**'},
+            {
+              protocol: 'http',
+              hostname: 'images-assets.nasa.gov',
+              port:'',
+              pathname: '/**'  
+            }
+        ]
+    }
+};
 
 export default nextConfig;
