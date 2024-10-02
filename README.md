ecc17b2fed136559b8e316f241ed6694cca7726b
lucas-corradini <lucas.corradini.2003@gmail.com>
2024-10-01 22:08:52 -0300

    CP devidamente finalizado

---
+++ README.md
@@ -0,0 +1,5 @@
+Felipe Seiki Hashiguti RM98985
+Lucas Corradini Silveira RM555118
+Matheus Gregorio Mota RM557254
+
+Link GitHub: https://github.com/MatheusGmota/CP5-Front
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
         <h1 className='text-5xl p-3'>
             ERROR 404 - Not Found
         </h1>
-        <button type='button' className='cursor-pointer bg-[#1da1f2] text-white text-2xl cursor-pointer rounded w-48 h-12 m-4'>
+        <button type='button' className='cursor-pointer bg-[#1da1f2] text-white text-2xl rounded w-48 h-12 m-4'>
           Voltar Home
         </button>
       </section>
