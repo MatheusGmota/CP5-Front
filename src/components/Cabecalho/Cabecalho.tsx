5b698c5ada3b7b718d59b8e69daf88830247b2bb
MatheusGmota <livematgreg@gmail.com>
2024-10-01 21:48:35 -0300

    Alterações no estilo do cabecalho

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -2,7 +2,7 @@ import Menu from "../Menu/Menu";
 
 export default function Cabecalho() {
   return (
-    <header className="fixed flex justify-center items-center w-full h-20 p-3 bg-slate-800">
+    <header className="fixed top-0 flex justify-center items-center w-full h-20 p-3 bg-slate-800">
         <Menu/>
     </header>
   )
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,10 +4,10 @@ export default function Menu() {
   return (
     <nav className="flex justify-center items-center w-full">
         <ul className="flex justify-between items-center w-1/2">
-            <li><Link className="font-semibold hover:underline" href="/" >Home</Link></li>
-            <li><Link className="font-semibold hover:underline" href="/introducao" >Introdução</Link></li>
-            <li><Link className="font-semibold hover:underline" href="/ideias" >Principais Ideias</Link></li>
-            <li><Link className="font-semibold hover:underline" href="/conclusao" >Conclusão</Link></li>
+            <li><Link className="text-white font-semibold hover:underline" href="/" >Home</Link></li>
+            <li><Link className="text-white font-semibold hover:underline" href="/introducao" >Introdução</Link></li>
+            <li><Link className="text-white font-semibold hover:underline" href="/ideias" >Principais Ideias</Link></li>
+            <li><Link className="text-white font-semibold hover:underline" href="/conclusao" >Conclusão</Link></li>
         </ul>
     </nav>
   )
