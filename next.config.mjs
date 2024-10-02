f209a02eeac73748d9b9140b83d22222b0211064
MatheusGmota <livematgreg@gmail.com>
2024-10-01 21:22:20 -0300

    Adicionando pattern da api da nasa

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-        remotePatterns: [
-            {
-              protocol: 'https', 
-              hostname: 'http.cat', 
-              port: '', 
-              pathname: '/**'
-            },
-            {
-              protocol: 'http',
-              hostname: 'images-assets.nasa.gov',
-              port:'',
-              pathname: '/**'  
-            }
-        ]
-    }
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', 
+        hostname: 'http.cat', 
+        port: '', 
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-assets.nasa.gov',
+        port:'',
+        pathname: '/**'  
+      },
+      {
+        protocol: 'http',
+        hostname: 'images-assets.nasa.gov',
+        port:'',
+        pathname: '/**'  
+      }
+    ]
+  }
+}
 
 export default nextConfig;
